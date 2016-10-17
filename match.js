fdbf4cf3cec280074ba07e6e6c73b561264f3a44
deepak6sp <deepak6sp@gmail.com>
2016-10-17 23:22:44 +1100

    reg exp modified

--- match.js
+++ match.js
@@ -3,8 +3,10 @@ module.exports.match = function(hash) {
   // Write your solution to Task #2 - Match Metadata task here
 	var jsonObj = hash;
 	var array = [];
+	var regex = new RegExp(/^(card)\s(expire)\s(date)\s[a-z]\w+/);
+	
 	Object.keys(jsonObj).forEach(function(key) {
- 		if(/^card expire date day|card expire date month|card expire date year/.test(key)){
+ 		if(regex.test(key)){
 	    	array.push(key);
  		}
 
